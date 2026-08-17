6916d46440b2bb5c8fa8726eea00df03b5703c9e
CrossXCodes <crossxcodes911@gmail.com>
2026-08-17 22:05:55 +0530

    Practical 2A Added

---
+++ DSU/Experiment 2/Practical 2A/Program 1.cpp
@@ -0,0 +1,19 @@
+/* Code by Anom Meshram
+Class: SY CSE, Div: B, RollNo: 21 */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a, new_de, n;
+    cout << "Enter a number ";
+    cin >> a;
+    cout << "Enter position to toggle: ";
+    cin >> n;
+    
+    int t = n - 1;
+    new_de = a ^ 1<<t;
+    cout << "toggled output number: " << new_de << endl;
+    
+    
+    return 0;
+}
